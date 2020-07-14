4363fe48d1b18f39b00d1b7c1c3749224bb728cc
Harry <conkerdonk02@gmail.com>
2020-07-14 23:56:36 +0100

    asd

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 $("button").click(function(){
-    console.log('Console . bruh more like');
-    $.get("http://api.reddit.com/r/dog", function(data){
+    $.get("http://api.reddit.com/r/"+$('.input1').val(), function(data){
         $('.pictures').text('');
         for(var i = 0; i < data.data.children.length; i++){
             $('.pictures').prepend('<img id="theImg" width="250" height="250" src="'+data.data.children[i].data.thumbnail+'" />');
