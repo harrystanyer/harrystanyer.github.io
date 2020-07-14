72e7db61b4cc97b67aa8872aba5b39bd45dde28f
Harry <conkerdonk02@gmail.com>
2020-07-14 22:48:23 +0100

    asd

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('asdasd');
+$("button").click(function(){
+    console.log('Console . bruh more like');
+    $.get("http://api.reddit.com/r/aww", function(data){
+        console.log(data.data.children[0].data.thumbnail);
+        //$('#theDiv').append('<img id="theImg" src="'+data.data.children[0].data.thumbnail+'" />')
+      });
+  });
