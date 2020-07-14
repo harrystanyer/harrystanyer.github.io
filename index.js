18fdf161b49000aa70f7e6d5d06e1778e4bf67dd
Harry <conkerdonk02@gmail.com>
2020-07-14 22:52:56 +0100

    ye

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ $("button").click(function(){
     console.log('Console . bruh more like');
     $.get("http://api.reddit.com/r/aww", function(data){
         console.log(data.data.children[0].data.thumbnail);
-        //$('#theDiv').append('<img id="theImg" src="'+data.data.children[0].data.thumbnail+'" />')
+        //$('#theDiv').prepend('<img id="theImg" src="'+data.data.children[0].data.thumbnail+'" />')
+        $('h1').hide();
       });
   });
