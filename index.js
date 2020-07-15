5d279b32ecfac5372d7577062d09e0a1ac032a50
harrystanyer <52531229+harrystanyer@users.noreply.github.com>
2020-07-15 11:00:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $("button").click(function(){
-    $.get("http://api.reddit.com/r/"+$('.input1').val(), function(data){
+    $.get("https://api.reddit.com/r/"+$('.input1').val(), function(data){
         $('.pictures').text('');
         for(var i = 0; i < data.data.children.length; i++){
             $('.pictures').prepend('<img id="theImg" width="250" height="250" src="'+data.data.children[i].data.thumbnail+'" />');
