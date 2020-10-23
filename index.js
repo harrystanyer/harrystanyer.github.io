2a5eb75fd048d246e90a9179a4b53fc1e0a5a19a
Harry <conkerdonk02@gmail.com>
2020-10-23 23:03:22 +0100

    Api integration for useless facts

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 $(document).ready(function () {
-    $("#navHide").click(function () {
-        navHide();
+    $("#button1").click(function () {
+        getUselessFact();
     });
 });
 
-var navOpen = true;
-
-function navHide(){
-    if(navOpen){
-        navOpen = false;
-        $(".navbar").css("width","50px");
-        $("#home").html("+");
-        $("#about").html("+");
-        $("#githubButton").html("+");
-        $(".content").css("margin-left","50px");
-    }else{
-        navOpen = true;
-        $(".navbar").css("width","200px");
-        $("#home").html("Home");
-        $("#about").html("About");
-        $("#githubButton").html("Github");
-        $(".content").css("margin-left","150px");
-    }
+function firstWord(paragraph) {
+    var a = paragraph.Split(' ');
 }
 
-function firstWord(paragraph){
-    var a = paragraph.Split(' ');
+function getUselessFact() {
+    fetch('https://uselessfacts.jsph.pl/random.json')
+        .then((resp) => resp.json())
+        .then(function (data) {
+            $("#uselessFactTarget").html(data.text);
+        })
+        .catch(function () {
+            console.log('Not working');
+        });
 }
